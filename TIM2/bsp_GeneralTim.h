4cf62fc4a48c59c85b5d4d9769e155a1684774ca
RiverrWang <51364253+RiverrWang@users.noreply.github.com>
2019-07-31 20:01:54 +0800

    TIM2的配置
    使用TIM2的CH1和CH2 产生两路PWM分别控制左轮和右轮

---
+++ TIM2/bsp_GeneralTim.h
@@ -0,0 +1,36 @@
+#ifndef __BSP_GENERALTIME_H
+#define __BSP_GENERALTIME_H
+
+
+#include "stm32f10x.h"
+
+
+/************通用定时器TIM参数定义，只限TIM2、3、4、5************/
+// 当使用不同的定时器的时候，对应的GPIO是不一样的，这点要注意
+// 我们这里默认使用TIM3
+
+#define            GENERAL_TIM                   TIM2
+#define            GENERAL_TIM_APBxClock_FUN     RCC_APB1PeriphClockCmd
+#define            GENERAL_TIM_CLK               RCC_APB1Periph_TIM2
+#define            GENERAL_TIM_Period            99
+#define            GENERAL_TIM_Prescaler         71
+
+
+// TIM4 输出比较通道3
+#define            GENERAL_TIM_CH1_GPIO_CLK      RCC_APB2Periph_GPIOA
+#define            GENERAL_TIM_CH1_PORT          GPIOA
+#define            GENERAL_TIM_CH1_PIN           GPIO_Pin_0
+
+// TIM4 输出比较通道4
+#define            GENERAL_TIM_CH2_GPIO_CLK      RCC_APB2Periph_GPIOA
+#define            GENERAL_TIM_CH2_PORT          GPIOA
+#define            GENERAL_TIM_CH2_PIN           GPIO_Pin_1
+
+/**************************函数声明********************************/
+
+
+void 	GENERAL_TIM_GPIO_Config();
+void GENERAL_TIM_Mode_Config(	uint16_t CCR1_Val ,uint16_t CCR2_Val );
+#endif	/* __BSP_GENERALTIME_H */
+
+
